c72be181bf028b01790da29823732f1dc52a6399
Asluf <ahmedasluf@gmail.com>
2024-03-29 15:24:51 +0530

    chnaged cors

--- note-app-backend/index.js
+++ note-app-backend/index.js
@@ -14,20 +14,13 @@ const mongoose = require("mongoose");
 
 // app.use(cors(corsOptions));
 // app.use(cors());
-// app.use(cors(
-//   {
-//     origin: ["https://note-app-frontend-nine.vercel.app"],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true
-//   }
-// ))
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors(
+  {
+    origin: ["https://note-app-frontend-nine.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  }
+))
 
 
 var port = process.env.PORT || 6000;
